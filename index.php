f557b397c49a7eec24d81ae263de966ff30a6cfd
Malikabhc <malika.bouhcine@gmail.com>
2018-07-30 14:33:55 +0200

    exercice2-4

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2/4</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        function st($text){
+          return $text;
+        }
+        echo st('Bonjour');
+      ?>
+    </p>
+  </body>
+</html>
